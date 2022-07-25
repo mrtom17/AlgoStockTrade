0467d43aff755f0e962e44323cbcea4a48a79a9c
tom17 <tom17@kakao.com>
2022-07-25 22:23:45 +0900

    logic modify

--- AlgoTrade2.py
+++ AlgoTrade2.py
@@ -191,7 +191,7 @@ def _check_profit():
             stock_psbl_qty = mystocklist.iloc[i]['매도가능수량']
             stock_cur_price = mystocklist.iloc[i]['현재가']
             profit_percent = mystocklist.iloc[i]['수익율']
-            if profit_percent > 4.0:
+            if profit_percent > 4.8:
                 stocks.append({'sell_code': stock_code, 'sell_qty': stock_psbl_qty,'sell_price': stock_cur_price})
             time.sleep(1)
         return stocks
@@ -212,10 +212,10 @@ def _sell_each_stock(stocks):
 
                 ret = atof.do_sell(s['sell_code'], s['sell_qty'], current_price)
                 if ret:
-                    msgout('변동성 돌파 매도 주문(이익율 3% 달성) 성공 ->('+str(s['code'])+')('+str(current_price)+')')
+                    msgout('변동성 돌파 매도 주문(이익율 4.8% 달성) 성공 ->('+str(s['code'])+')('+str(current_price)+')')
                     sell_done_list.append(s['sell_code'])
                 else:
-                    msgout('변동성 돌파 매도 주문(이익율 3% 달성) 실패 ->('+str(s['code'])+')')
+                    msgout('변동성 돌파 매도 주문(이익율 4.8% 달성) 실패 ->('+str(s['code'])+')')
 
             time.sleep(1)
     except Exception as ex:
